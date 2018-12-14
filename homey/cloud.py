87a29e88aeae024c1c501c0299c07f6295035319
RoyK <rkokk@protonmail.com>
2018-12-15 00:49:26 +0100

    Added User info & authorization using OATH

--- homey/cloud.py
+++ homey/cloud.py
@@ -1,6 +1,7 @@
 import logging
 
-from homey.utils import create_url
+from homey.token import Token
+from homey.utils import create_url, get, post
 
 log = logging.getLogger(__name__)
 
@@ -26,6 +27,36 @@ class AthomCloudAPI:
         }
 
         return create_url("https://api.athom.com/oauth2/authorise", params)
+
+
+    def authenticateWithAuthorizationCode(self, oauth):
+        url = "https://api.athom.com/oauth2/token"
+
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+
+        data = {
+            'client_id': self.clientId,
+            'client_secret': self.clientSecret,
+            'grant_type': 'authorization_code',
+            'code': oauth
+        }
+
+        r = post(url, data=data, headers=headers)
+        self.token = Token.generate_token(r)
+
+
+    def getUser(self):
+        url = "https://api.athom.com/user/me"
+
+        headers = {
+            'Content-Type': 'application/json'
+        }
+
+        me = get(url, token=self.token, headers=headers)
+        log.debug(me)
+
 
     def setToken(self, token):
         self.token = token
