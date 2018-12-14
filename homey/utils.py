e98dd03b3ba29b22247541aec54092210ac67b0f
RoyK <rkokk@protonmail.com>
2018-12-14 23:15:44 +0100

    Added logging setup in utils.py

--- homey/utils.py
+++ homey/utils.py
@@ -1,4 +1,8 @@
+import sys
+import logging
 import requests
+
+log = logging.getLogger(__name__)
 
 def post(url, data):
     r = requests.post(
@@ -10,3 +14,22 @@ def post(url, data):
         raise Exception()
 
     return r.text
+
+def setup_logging(debug=False):
+
+    default_formatter = logging.Formatter(
+        "%(asctime)s.%(msecs)03d [%(levelname)-.1s]: %(message)s",
+        "%H:%M:%S"
+    )
+
+    r_log = logging.getLogger('homey')
+
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setFormatter(default_formatter)
+
+    if debug:
+        r_log.setLevel(logging.DEBUG)
+        ch.setLevel(logging.DEBUG)
+
+    r_log.addHandler(ch)
+
