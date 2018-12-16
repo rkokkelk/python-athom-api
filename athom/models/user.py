4868f7a65fb85fa9751c3b0ba8c65be7e524c1d8
RoyK <rkokk@protonmail.com>
2018-12-16 16:32:32 +0100

    Finished implemented functions in User model

--- athom/models/user.py
+++ athom/models/user.py
@@ -15,3 +15,33 @@ class User:
 
     def __str__(self):
         return "[{self._id}] {self.firstname} {self.lastname}".format(self=self)
+
+
+    def getDevice(self, d_id):
+
+        for device in self.devices:
+            if device._id == d_id:
+                return device
+
+        raise LookupError()
+
+
+    def getHomeys(self):
+        return self.homeys
+
+
+    def getHomeyById(self, h_id):
+
+        for homey in self.homeys:
+            if homey._id == h_id:
+                return homey
+
+        raise LookupError()
+
+
+    def getFirstHomey(self):
+
+        if self.homeys:
+            return self.homeys[0]
+
+        raise LookupError()
