3753d4f7af2bc0a7ac0c34d67eae4cbdd71715f3
RoyK <rkokk@protonmail.com>
2018-12-16 16:47:45 +0100

    Initial setup of Athom Web-API scopes file

---
+++ athom/common/scopes.py
@@ -0,0 +1,16 @@
+class homeySpeech:
+
+    def __str__(self):
+        return "homey.speech"
+
+
+class homeyLogicReadonly:
+
+    def __str__(self):
+        return "homey.logic.readonly"
+
+
+class homeySystem:
+
+    def __str__(self):
+        return "homey.system"
