2bb1ac9bebfe1dc2173be7c6896d75df301d27a5
RoyK <rkokk@protonmail.com>
2018-12-31 14:14:16 +0100

    Added logging & authenticate to Homey model

--- athom/models/homey.py
+++ athom/models/homey.py
@@ -1,3 +1,9 @@
+import logging
+
+from athom.homey import HomeyAPI
+
+log = logging.getLogger(__name__)
+
 class Homey:
 
     def __init__(self, _id, name, ipInternal, softwareVersion,
@@ -15,9 +21,21 @@ class Homey:
         self.token = token
         self.apps = apps
 
+
     def __str__(self):
         return "[{self._id}] {self.name} ({self.softwareVersion})".format(self=self)
 
 
-    def authenticate(self):
-        raise NotImplementedError()
+    def authenticate(self, token=None, strategy=None):
+
+        if strategy and strategy != "cloud":
+            message = "This module can only be used in cloud modus"
+            log.error(message)
+            raise ValueError(message)
+
+        if not token and not self.token:
+            message = "A token is required in order to access HomeyAPI"
+            log.error(message)
+            raise ValueError(message)
+
+        return HomeyAPI(self.ipInternal, token=self.token)
