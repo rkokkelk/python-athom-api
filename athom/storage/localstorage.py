f360f6a5ea66ee700b0568aa03027779b87e245a
RoyK <rkokk@protonmail.com>
2018-12-31 14:14:45 +0100

    Increased  handeling of issues with LocalStorage

--- athom/storage/localstorage.py
+++ athom/storage/localstorage.py
@@ -1,5 +1,10 @@
+import os
 import json
 import logging
+
+from athom.common.exceptions import AthomTokenDBError
+
+from json.decoder import JSONDecodeError
 
 log = logging.getLogger(__name__)
 
@@ -8,10 +13,11 @@ class LocalStorage:
     def __init__(self, path="homeyLocalStorage.db"):
 
         self.path = path
-        self.storage = dict()
 
-        with open(self.path, 'r') as f:
-            self.storage = json.load(f)
+        try:
+            self.storage = self.__parsedb__()
+        except AthomTokenDBError:
+            self.storage = dict()
 
 
     def set(self, key, value):
@@ -22,12 +28,22 @@ class LocalStorage:
 
 
     def get(self, key):
-
-        with open(self.path, 'r') as f:
-            self.storage = json.load(f)
-
+        self.storage = self.__parsedb__()
         return self.storage[key]
 
 
     def __contains__(self, m):
         return self.storage is not None and m in self.storage
+
+
+    def __parsedb__(self):
+
+        try:
+            with open(self.path, 'r') as f:
+                return json.load(f)
+
+        except (FileNotFoundError, JSONDecodeError) as e:
+            message = "Failed to retrieve token from: {self.path}".format(self=self)
+
+            log.error(message)
+            raise AthomTokenDBError(message)
