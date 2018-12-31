a8363c05e0b96d0d3ac9429d5a2b7a2eee834b5e
RoyK <rkokk@protonmail.com>
2018-12-31 14:09:46 +0100

    Extended parsing of Homey error messages

--- athom/common/exceptions.py
+++ athom/common/exceptions.py
@@ -6,9 +6,15 @@ class AthomAPIError(Exception):
     def __init__(self, error_content):
         error_dict = json.loads(error_content)
 
-        self.code = error_dict['code']
-        self.error = error_dict['error']
-        self.description = error_dict['error_description']
+        if 'code' in error_dict:
+            self.code = error_dict['code']
+            self.error = error_dict['error']
+            self.description = error_dict['error_description']
+
+        else:
+            self.code = error_dict['status']
+            self.error = error_dict['result']
+            self.description = 'Unknown error'
 
     def __str__(self):
         return "[{self.code}] {self.error}: {self.description}".format(self=self)
