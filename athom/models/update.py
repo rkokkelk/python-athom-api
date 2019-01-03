55835fe1035b74d99b44c1213072096fadb67780
RoyK <rkokk@protonmail.com>
2019-01-03 07:37:01 +0100

    Added gt/lt/eq functions to Update model

--- athom/models/update.py
+++ athom/models/update.py
@@ -17,3 +17,45 @@ class Update:
 
     def __str__(self):
         return "Update {self.version}".format(self=self)
+
+
+    def __eq__(self, other):
+        return self.version == other.version
+
+
+    def __lt__(self, other):
+        s_split = self.version.split('-rc')
+        o_split = other.version.split('-rc')
+
+        # 2.0.0-rc1 < 2.0.0
+        if s_split[0] == o_split[0]:
+
+            # 2.0.0-rc1 < 2.0.0-rc20
+            if len(s_split) == 2 and len(o_split) == 2:
+                return int(s_split[1]) < int(o_split[1])
+
+            else:
+                return self.version < other.version
+
+        else:
+            # 1.5.3 < 2.0.0-rc10
+            return self.version < other.version
+
+
+    def __gt__(self, other):
+        s_split = self.version.split('-rc')
+        o_split = other.version.split('-rc')
+
+        # 2.0.0-rc1 > 2.0.0
+        if s_split[0] == o_split[0]:
+
+            # 2.0.0-rc1 > 2.0.0-rc20
+            if len(s_split) == 2 and len(o_split) == 2:
+                return int(s_split[1]) > int(o_split[1])
+
+            else:
+                return self.version > other.version
+
+        else:
+            # 1.5.3 > 2.0.0-rc10
+            return self.version > other.version
