5343320ddc97942aa42178d15769d563c5a2edb7
RoyK <rkokk@protonmail.com>
2019-01-05 18:05:10 +0100

    Added handling for no API session

--- athom/token.py
+++ athom/token.py
@@ -1,14 +1,16 @@
 import json
 import logging
 
-from athom.common.net import get, post
+from athom.common.net import post
+from athom.common.exceptions import AthomCloudAuthenticationError, \
+                                    AthomAPISessionError
 
 log = logging.getLogger(__name__)
 
 class Token:
 
     def __init__(self, athom, **kwargs):
-        self.access_token = kwargs.get('access_token', None)
+        self._access_token = kwargs.get('access_token', None)
         self.expires_in = kwargs.get('expires_in', -1)
         self.token_type = kwargs.get('token_type', 'bearer')
         self.refresh_token = kwargs.get('refresh_token', None)
@@ -16,9 +18,22 @@ class Token:
         self.athom = athom
 
 
+    @property
+    def access_token(self):
+        if self._access_token is None:
+            raise AthomAPISessionError()
+
+        return self._access_token
+
+
+    @access_token.setter
+    def access_token(self, token):
+        self._access_token = token
+
+
     def jsonify(self):
         columns = ['access_token', 'expires_in', 'token_type', 'refresh_token']
-        return {k:v for k, v in self.__dict__.items() if k in columns}
+        return {k:getattr(self, k) for k in columns}
 
 
     def refresh(self):
@@ -35,8 +50,14 @@ class Token:
             'refresh_token': self.refresh_token
         }
 
-        log.info("Refreshing token")
-        r = post(url, data=data, headers=headers)
+        try:
+            log.info("Refreshing token")
+            r = post(url, data=data, headers=headers, refresh=False)
+
+        except AthomCloudAuthenticationError:
+            self.destroy()
+            log.error("OAUTH2 session has expired, required to login again!")
+            raise AthomAPISessionError()
 
         data = json.loads(r)
         for key, value in data.items():
@@ -45,10 +66,21 @@ class Token:
         self.athom.storage.set('token', self.jsonify())
 
 
+    def destroy(self):
+        self._access_token = None
+        self.expires_in = -1
+        self.token_type = None
+        self.refresh_token = None
+
+
     def __str__(self):
         """ Standard string representation. Access_token defines Token object
         """
-        return self.access_token
+        return self._access_token
+
+
+    def __bool__(self):
+        return self._access_token is not None
 
 
     @staticmethod
