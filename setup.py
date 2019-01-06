4224f60dd6556c31a545b52ba0ce28e0d66f1167
RoyK <rkokk@protonmail.com>
2019-01-06 23:20:37 +0100

    Updated setup.py, bumped to version 0.2.1
    * Minimised requirements
    * Added proper format of readme
    * Extended classifiers

--- setup.py
+++ setup.py
@@ -1,26 +1,32 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
-
 setup(
     name="athom-api",
-    version="0.2.0",
+    version="0.2.1",
     author="Roy K",
     author_email="rkokk@protonmail.com",
-    description=("A python module for accessing the Athom Web-API for use with Athom Homey"),
+    description=("A python module for accessing the Athom Web-API for use with the Athom Homey"),
     license="GPLv3",
     keywords="API athom homey",
     url="https://github.com/rkokkelk/python-athom-api",
-    install_requires=requirements,
+    install_requires=[
+        'configparser==3.5.0',
+        'marshmallow==3.0.0rc1',
+        'requests==2.21.0'
+    ],
     long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(exclude=['contrib', 'docs', 'env*', 'tests*']),
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Topic :: Utilities"
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Home Automation'
     ],
     entry_points={
         'console_scripts': [
