f901b9207b5b99a5216fbbb7c758cf77d08ef75e
RoyK <rkokk@protonmail.com>
2019-01-07 01:06:31 +0100

    Added APP related scopes

--- athom/common/scopes.py
+++ athom/common/scopes.py
@@ -1,3 +1,7 @@
+HOMEY_APP = 'homey.app'
+HOMEY_APP_READONLY = 'homey.app.readonly'
+HOMEY_APP_CONTROL = 'homey.app.control'
+
 HOMEY_LOGIC_READONLY = 'homey.logic.readonly'
 HOMEY_SPEECH = 'homey.speech'
 HOMEY_SYSTEM = 'homey.system'
