8ff0f2be1387c01a58d111aa6ab1b68611bc9417
RoyK <rkokk@protonmail.com>
2019-01-11 19:11:31 +0100

    Added DEVICE related scopes

--- athom/common/scopes.py
+++ athom/common/scopes.py
@@ -2,6 +2,10 @@ HOMEY_APP = 'homey.app'
 HOMEY_APP_READONLY = 'homey.app.readonly'
 HOMEY_APP_CONTROL = 'homey.app.control'
 
+HOMEY_DEVICE = 'homey.device'
+HOMEY_DEVICE_CONTROL = 'homey.device.control'
+HOMEY_DEVICE_READONLY = 'homey.device.readonly'
+
 HOMEY_LOGIC_READONLY = 'homey.logic.readonly'
 HOMEY_SPEECH = 'homey.speech'
 HOMEY_SYSTEM = 'homey.system'
