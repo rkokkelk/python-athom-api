1f5d068bb0cae389b8c61ee592cba43ef9005aca
RoyK <rkokk@protonmail.com>
2020-05-02 19:08:45 +0200

    Rewrite net to AthomSession to use request.Session
    To use request.Session results in more code reuse. Additionally all
    subsequent requests are kept in active TCP connection, thus connection
    reuse.

--- athom/common/net.py
+++ athom/common/net.py
@@ -1,6 +1,6 @@
 import logging
 
-import requests
+from requests import Session
 from requests.exceptions import ConnectionError, SSLError
 
 from athom.common.exceptions import AthomCloudAuthenticationError, \
@@ -10,139 +10,62 @@ from athom.common.exceptions import AthomCloudAuthenticationError, \
 
 log = logging.getLogger(__name__)
 
-# Timeout for (connect, read)
-TIMEOUT = (4, 30)
 
+class AthomSession(Session):
 
+    def __init__(self, **kwargs):
+        self.token = kwargs.pop('token')
+        self.base = kwargs.pop('base', '')
 
-def post(url, refresh=True, **kwargs):
-    log.debug('POST: %s', url)
-    token = kwargs.get('token', None)
+        super().__init__(**kwargs)
 
-    try:
-        return _request(
-            requests.post,
-            url,
-            **kwargs
-        )
+    def prepare_request(self, request):
+        request.url = self.base + request.url
+        request.headers['authorization'] = f"Bearer {self.token}"
+        request.timeout = (4, 30)  # Timeout for (connect, read)
 
-    except AthomCloudAuthenticationError as e:
+        return super().prepare_request(request)
 
-        # If authentication error, try to refresh token once
-        if not isinstance(token, str) and not (refresh and token and token.refresh_token):
-            raise e
+    def request(self, method, url, **kwargs):
+        log.debug('%s: %s', method, url)
+        refresh = kwargs.pop('refresh', True)
 
-        token.refresh()
+        try:
+            r = super().request(method, url, **kwargs)
+            return self._parse_response(r.status_code, r)
 
-        return post(url, refresh=False, **kwargs)
+        except (ConnectionError, SSLError) as e:
+            error = AthomAPIConnectionError(e)
+            log.critical(error)
+            raise error
 
+        except AthomCloudAuthenticationError as e:
 
-def delete(url, refresh=True, **kwargs):
-    log.debug('DELETE: %s', url)
-    token = kwargs.get('token', None)
+            # If authentication error, try to refresh token once
+            if not (refresh and self.token and self.token.refresh_token):
+                raise e
 
-    try:
-        return _request(
-            requests.delete,
-            url,
-            **kwargs
-        )
+            self.token.refresh()
 
-    except AthomCloudAuthenticationError as e:
+            r = super().request(method, url, **kwargs)
+            return self._parse_response(r.status_code, r)
 
-        # If authentication error, try to refresh token once
-        if not isinstance(token, str) and not (refresh and token and token.refresh_token):
-            raise e
+    def _parse_response(self, status_code, response):
+        text = response.text
 
-        token.refresh()
+        if status_code == 200:
+            return text
 
-        return delete(url, refresh=False, **kwargs)
+        if status_code in (400, 401):
+            error = AthomCloudAuthenticationError(text)
+            log.warning(error)
 
+        elif status_code == 502:
+            error = AthomCloudGateWayAPIError()
+            log.warning(error)
 
-def get(url, refresh=True, **kwargs):
-    log.debug('GET: %s', url)
-    token = kwargs.get('token', None)
+        else:
+            error = AthomCloudUnknownAPIError(text)
+            log.error(error)
 
-    try:
-        return _request(
-            requests.get,
-            url,
-            **kwargs
-        )
-
-    except AthomCloudAuthenticationError as e:
-
-        # If authentication error, try to refresh token once
-        if not isinstance(token, str) and not (refresh and token and token.refresh_token):
-            raise e
-
-        token.refresh()
-
-        return get(url, refresh=False, **kwargs)
-
-
-def put(url, refresh=True, **kwargs):
-    token = kwargs.get('token', None)
-
-    try:
-        return _request(
-            requests.put,
-            url,
-            **kwargs
-        )
-
-    except AthomCloudAuthenticationError as e:
-
-        # If authentication error, try to refresh token once
-        if not isinstance(token, str) and not (refresh and token and token.refresh_token):
-            raise e
-
-        token.refresh()
-
-        return put(url, refresh=False, **kwargs)
-
-
-
-
-def _request(method, url, token=None, **kwargs):
-    headers = kwargs.pop('headers', dict())
-
-    # Verify on None, empty tokens will properly raise error in str(token)
-    if token is not None:
-        headers['authorization'] = "Bearer {}".format(token)
-
-    try:
-        r = method(
-            url=url,
-            timeout=TIMEOUT,
-            headers=headers,
-            **kwargs
-        )
-
-        return _parse_response(r.status_code, r)
-
-    except (ConnectionError, SSLError) as e:
-        error = AthomAPIConnectionError(e)
-        log.critical(error)
         raise error
-
-
-def _parse_response(status_code, response):
-    text = response.text
-
-    if status_code == 200:
-        return text
-
-    if status_code in (400, 401):
-        error = AthomCloudAuthenticationError(text)
-        log.warning(error)
-
-    elif status_code == 502:
-        error = AthomCloudGateWayAPIError()
-        log.warning(error)
-
-    else:
-        error = AthomCloudUnknownAPIError(text)
-        log.error(error)
-
-    raise error
