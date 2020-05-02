6d239c4f155fccc2981f18fea2f8c8c6f2d51d1e
RoyK <rkokk@protonmail.com>
2020-05-02 19:37:50 +0200

    Modify ManagerSpeech to use AthomSession

--- athom/managers/speech.py
+++ athom/managers/speech.py
@@ -1,29 +1,25 @@
 from athom.common import scopes
-from athom.common.net import post
 from athom.managers.manager import Manager
+
 
 class ManagerSpeechInput(Manager):
 
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+        super().__init__(
+            base=f"http://{self.homey.ip}/api/manager/speech-input",
+            **kwargs
+        )
 
-        self.path = "http://{homey.ip}/api/manager/speech-input".format(homey=self.homey)
         self.requiredScopes = scopes.HOMEY_SPEECH
 
-
     def parseSpeech(self, transcript=None, **opts):
-        url = self.path+"/"
-
         if not transcript:
             raise ValueError("Missing required transcript parameter")
 
-        return post(url, json={'transcript': transcript, **opts}, token=self.token)
-
+        return self.s.post('/', json={'transcript': transcript, **opts})
 
     def ask(self, text=None, **opts):
-        url = self.path+"/ask"
-
         if not text:
             raise ValueError("Missing required text parameter")
 
-        return post(url, json={'text': text, **opts}, token=self.token)
+        return self.s.post('/ask', json={'text': text, **opts})
