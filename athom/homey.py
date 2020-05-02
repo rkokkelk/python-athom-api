d721663b2f2746db985ea92409fdbd1091f93a02
RoyK <rkokk@protonmail.com>
2020-05-03 00:05:27 +0200

    Update creation of Managers in HomeyAPI

--- athom/homey.py
+++ athom/homey.py
@@ -6,18 +6,16 @@ from athom.managers.speech import ManagerSpeechInput
 class HomeyAPI:
 
     def __init__(self, url, token):
-
         self.url = url
+
         # Use delegationToken to create sessionToken to access API
-        self.ManagerUsers = ManagerUsers(homey=self, token=token)
+        self.ManagerUsers = ManagerUsers(homey=self)
         self.token = self.ManagerUsers.login(token)
 
         # Ensure to get a valid Session
-        #self.ManagerSessions = ManagerSessions(homey=self, token=self.token)
-        #self.ManagerSessions.getSessionMe()
-
-        #self.ManagerApps = ManagerApps(homey=self, token=self.token)
-        #self.ManagerSpeechInput = ManagerSpeechInput(homey=self, token=self.token)
+        self.ManagerApps = ManagerApps(homey=self, token=self.token)
+        self.ManagerSessions = ManagerSessions(homey=self, token=self.token)
+        self.ManagerSpeechInput = ManagerSpeechInput(homey=self, token=self.token)
 
 
     def forCurrentHomey(self):
