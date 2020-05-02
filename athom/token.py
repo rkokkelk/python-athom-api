a9ae710a5c7f85776450f387ce06f230400d1054
RoyK <rkokk@protonmail.com>
2020-05-03 00:03:49 +0200

    Change Token class to use requests.Session

--- athom/token.py
+++ athom/token.py
@@ -1,11 +1,12 @@
 import json
+import requests
 import logging
 
-from athom.common.net import post
 from athom.common.exceptions import AthomCloudAuthenticationError, \
                                     AthomAPISessionError
 
 log = logging.getLogger(__name__)
+
 
 class Token:
 
@@ -16,7 +17,7 @@ class Token:
         self.refresh_token = kwargs.get('refresh_token', None)
 
         self.athom = athom
-
+        self.api = requests.Session()
 
     @property
     def access_token(self):
@@ -25,16 +26,13 @@ class Token:
 
         return self._access_token
 
-
     @access_token.setter
     def access_token(self, token):
         self._access_token = token
 
-
     def jsonify(self):
         columns = ['access_token', 'expires_in', 'token_type', 'refresh_token']
-        return {k:getattr(self, k) for k in columns}
-
+        return {k: getattr(self, k) for k in columns}
 
     def refresh(self):
         url = "https://api.athom.com/oauth2/token"
@@ -52,19 +50,19 @@ class Token:
 
         try:
             log.info("Refreshing token")
-            r = post(url, data=data, headers=headers, refresh=False)
+            r = self.api.post(url, data=data, headers=headers)
+            r.raise_for_status()
 
         except AthomCloudAuthenticationError:
             self.destroy()
             log.error("OAUTH2 session has expired, required to login again!")
             raise AthomAPISessionError()
 
-        data = json.loads(r)
+        data = r.json()
         for key, value in data.items():
             setattr(self, key, value)
 
         self.athom.storage.set('token', self.jsonify())
-
 
     def destroy(self):
         self._access_token = None
@@ -72,16 +70,13 @@ class Token:
         self.token_type = None
         self.refresh_token = None
 
-
     def __str__(self):
         """ Standard string representation. Access_token defines Token object
         """
         return self.access_token
 
-
     def __bool__(self):
         return self._access_token is not None
-
 
     @staticmethod
     def generate_token(athom, json_str):
