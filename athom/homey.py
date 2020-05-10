6e9b331418673a54a3a8a10405104962fa13fc99
RoyK <rkokk@protonmail.com>
2020-05-10 18:12:16 +0200

    Rename managers in HomeyAPI

--- athom/homey.py
+++ athom/homey.py
@@ -3,20 +3,25 @@ from athom.managers.users import ManagerUsers
 from athom.managers.sessions import ManagerSessions
 from athom.managers.speech import ManagerSpeechInput
 
+
 class HomeyAPI:
 
     def __init__(self, url, token):
         self.url = url
 
         # Use delegationToken to create sessionToken to access API
-        self.ManagerUsers = ManagerUsers(homey=self)
+        self.users = ManagerUsers(homey=self)
+        # Ensure to get a valid Session
         self.token = self.ManagerUsers.login(token)
 
-        # Ensure to get a valid Session
-        self.ManagerApps = ManagerApps(homey=self, token=self.token)
-        self.ManagerSessions = ManagerSessions(homey=self, token=self.token)
-        self.ManagerSpeechInput = ManagerSpeechInput(homey=self, token=self.token)
+        manager_options = {
+            'homey': self,
+            'token': self.token
+        }
 
+        self.apps = ManagerApps(**manager_options)
+        self.session = ManagerSessions(**manager_options)
+        self.speechInput = ManagerSpeechInput(**manager_options)
 
     def forCurrentHomey(self):
         raise NotImplementedError("This function is not supported by this module")
