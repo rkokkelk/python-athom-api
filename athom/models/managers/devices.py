b34538381fbbc356450af037048d0a5090151546
RoyK <rkokk@protonmail.com>
2020-05-10 21:17:19 +0200

    Add Device model for ManagerDevices

---
+++ athom/models/managers/devices.py
@@ -0,0 +1,38 @@
+import logging
+
+from marshmallow import Schema, fields, post_load, EXCLUDE
+
+log = logging.getLogger(__name__)
+
+
+class Device:
+
+    def __init__(self, id=None, **kwargs):
+        self.id = id
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def __str__(self):
+        return f"Device [{self.zoneName}] {self.name}"
+
+
+class DeviceSchema(Schema):
+
+    # Specify vars which can have None values
+    icon = fields.Str(allow_none=True)
+    virtualClass = fields.Str(allow_none=True)
+    energy = fields.Str(allow_none=True)
+    unavailableMessage = fields.Str(allow_none=True)
+
+    class Meta:
+        additional = ['id', 'name', 'driverUri', 'driverId', 'zone', 'zoneName', 'iconObj',
+                      'settings', 'settingsOjb', 'class', 'energyObj',
+                      'ui', 'capabilities', 'capabilitiesObj', 'capabilitiesOptions', 'flags',
+                      'ready', 'available', 'repair', 'unpair', 'images',
+                      'insights', 'color']
+        unknown = EXCLUDE
+
+    @post_load
+    def create_obj(self, data):
+        return Device(**data)
